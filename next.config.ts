ca26ff9960eb5bab5f9e4de5dc1f9ae163752c7d
Jared <jvredmoore@gmail.com>
2024-11-03 22:15:38 -0500

    build fix

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,16 @@
 import type { NextConfig } from 'next';
 
 const isGithubActions = process.env.GITHUB_ACTIONS === 'true';
+const repo = 'whatmorecouldyouneed'; // Your repository name without .github.io
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   output: 'export',
-  basePath: isGithubActions ? '/whatmorecouldyouneed.github.io' : '',
-  assetPrefix: isGithubActions ? '/whatmorecouldyouneed.github.io/' : '',
+  basePath: isGithubActions ? `/${repo}` : '',
+  assetPrefix: isGithubActions ? `/${repo}/` : '',
+  trailingSlash: true,
   images: {
-    unoptimized: true, 
+    unoptimized: true,
   },
 };
 
