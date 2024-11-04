93088ecb77a7942efe0a8f1d4c6238351cec75b9
Jared <jvredmoore@gmail.com>
2024-11-04 00:55:58 -0500

    build fix

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,17 @@
 import type { NextConfig } from 'next';
 
 const isGithubActions = process.env.GITHUB_ACTIONS === 'true';
-const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  // For GitHub Pages, basePath and assetPrefix are needed to ensure assets load correctly
   basePath: isGithubActions ? '/wmcyn-online-front-end' : '',
   assetPrefix: isGithubActions ? '/wmcyn-online-front-end/' : '',
   images: {
-    unoptimized: true, // Disable Next.js image optimization for static builds
+    unoptimized: true, // Disable Next.js image optimization for GitHub Pages compatibility
   },
-  trailingSlash: true, // Ensures that all routes have trailing slashes, needed for GitHub Pages
-  output: 'export', // Export as static HTML files for GitHub Pages
-  env: {
-    CUSTOM_DOMAIN: isProd && !isGithubActions ? 'https://wmcyn.online' : `https://whatmorecouldyouneed.github.io/wmcyn-online-front-end`,
-  },
+  trailingSlash: true, // Ensures consistent routing for static site export
+  output: 'export', // Export the site as static HTML files
 };
 
 export default nextConfig;
